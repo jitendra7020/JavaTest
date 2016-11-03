0f7c90221dc447b25412f460bd0e7dd083ab83b7
Jitendra Nath <jitendra7020@gmail.com>
2016-11-03 06:08:45 +0530

    Java lib project created.

---
+++ java/src/main/java/com/example/MainClass.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * Created by admin on 03/11/16.
+ */
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        System.out.print("MainClass");
+    }
+}
